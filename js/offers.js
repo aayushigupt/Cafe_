896368da532ac6b26f0cc54662c2762aa8d37bdc
aayushigupt <aayushigupta108@gmail.com>
2021-07-12 01:04:47 +0530

    Changes

--- js/offers.js
+++ js/offers.js
@@ -1,12 +1,28 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.fromTo( ".offers-section", {
+gsap.fromTo(".offers-section", {
     backgroundColor: gsap.getProperty("html", "--black")
-  }, {
+}, {
     scrollTrigger: {
-      trigger: ".color-light",
-      scrub: true,
-     
+        trigger: ".color-light",
+        scrub: true,
+
     },
     backgroundColor: gsap.getProperty("html", "--brown")
-  });
+});
+
+gsap.timeline().fromTo(".heading", {
+    y: -5000,
+    duration: 6,
+}, {
+    y: 0,
+}).fromTo(".off", {
+    x: 5000,
+    duration: 3,
+}, {
+    x: 0,
+}).fromTo(".pl-5", {
+    y: 5000,
+}, {
+    y:0
+})
